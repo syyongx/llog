861ebb5eddc754d53ac565e1642c7428374bb299
inhere <in.798@qq.com>
2018-12-05 11:41:21 +0800

    add liscese file. fix golint check

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # llog
+
 llog - Monolog implementation in Go.
 
 ## Download & Installs
+
 ```
 go get github.com/syyongx/llog
 ```
 
 ## LICENSE
-llog source code is licensed under the [MIT](https://github.com/syyongx/llog/blob/master/LICENSE) Licence.
+
+llog source code is licensed under the [MIT](LICENSE) Licence.
--- logger.go
+++ logger.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// logger struct
+// Logger logger struct
 type Logger struct {
 	name       string
 	levels     map[int]string
@@ -27,7 +27,7 @@ var levels = map[int]string{
 	types.EMERGENCY: "emergency",
 }
 
-// new logger
+// NewLogger new logger
 func NewLogger(name string) *Logger {
 	return &Logger{
 		name:   name,
@@ -35,16 +35,17 @@ func NewLogger(name string) *Logger {
 	}
 }
 
+// GetName get name
 func (l *Logger) GetName() string {
 	return l.name
 }
 
-// Pushes a handler on to the stack.
+// PushHandler pushes a handler on to the stack.
 func (l *Logger) PushHandler(h types.IHandler) {
 	l.handlers = append([]types.IHandler{h}, l.handlers...)
 }
 
-// Pops a handler from the stack.
+// PopHandler Pops a handler from the stack.
 func (l *Logger) PopHandler() (types.IHandler, error) {
 	if len(l.handlers) < 1 {
 		return nil, errors.New("you tried to pop from an empty handler slice")
@@ -54,7 +55,7 @@ func (l *Logger) PopHandler() (types.IHandler, error) {
 	return first, nil
 }
 
-// Set handlers, replacing all existing ones.
+// SetHandlers Set handlers, replacing all existing ones.
 func (l *Logger) SetHandlers(handlers []types.IHandler) {
 	l.handlers = l.handlers[:0]
 	for i := len(handlers); i > 0; i-- {
@@ -62,17 +63,17 @@ func (l *Logger) SetHandlers(handlers []types.IHandler) {
 	}
 }
 
-// Get handlers
+// GetHandlers Get handlers
 func (l *Logger) GetHandlers() []types.IHandler {
 	return l.handlers
 }
 
-// Pushes a processor on to the stack.
+// PushProcessor Pushes a processor on to the stack.
 func (l *Logger) PushProcessor(p types.Processor) {
 	l.processors = append([]types.Processor{p}, l.processors...)
 }
 
-// Pops a processor from the stack.
+// PopProcessor Pops a processor from the stack.
 func (l *Logger) PopProcessor() (types.Processor, error) {
 	if len(l.processors) < 1 {
 		return nil, errors.New("you tried to pop from an empty processor stack")
@@ -82,12 +83,12 @@ func (l *Logger) PopProcessor() (types.Processor, error) {
 	return first, nil
 }
 
-// Get processors
+// GetProcessor Get processors
 func (l *Logger) GetProcessor() []types.Processor {
 	return l.processors
 }
 
-// Adds a log record.
+// AddRecord Adds a log record.
 func (l *Logger) AddRecord(level int, message string) (bool, error) {
 	hKey := -1
 	record := types.GetRecord()
@@ -125,12 +126,12 @@ func (l *Logger) AddRecord(level int, message string) (bool, error) {
 	return true, nil
 }
 
-// Gets all supported logging levels.
+// GetLevels Gets all supported logging levels.
 func (l *Logger) GetLevels() map[int]string {
 	return l.levels
 }
 
-// Gets the name of the logging level.
+// GetLevelName Gets the name of the logging level.
 func (l *Logger) GetLevelName(level int) (string, error) {
 	if v, ok := l.levels[level]; ok {
 		return v, nil
@@ -138,69 +139,70 @@ func (l *Logger) GetLevelName(level int) (string, error) {
 	return "", errors.New("level is not defined")
 }
 
-// Checks whether the Logger has a handler that listens on the given level.
+// IsHandling Checks whether the Logger has a handler that listens on the given level.
 func (l *Logger) IsHandling(level int) bool {
 	return true
 }
 
-// Logs with an arbitrary level.
+// Log Logs with an arbitrary level.
 func (l *Logger) Log(level int, message string) {
 	if _, ok := l.levels[level]; !ok {
 		//
 	}
+
 	l.AddRecord(level, message)
 }
 
-// Detailed debug information.
+// Debug Detailed debug information.
 func (l *Logger) Debug(message interface{}) {
 	l.AddRecord(types.DEBUG, l.String(message))
 }
 
-// Interesting events.
+// Info Interesting events.
 // Example: User logs in, SQL logs.
 func (l *Logger) Info(message interface{}) {
 	l.AddRecord(types.INFO, l.String(message))
 }
 
-// Normal but significant events.
+// Notice Normal but significant events.
 func (l *Logger) Notice(message interface{}) {
 	l.AddRecord(types.NOTICE, l.String(message))
 }
 
-// Exceptional occurrences that are not errors.
+// Warning Exceptional occurrences that are not errors.
 // Example: Use of deprecated APIs, poor use of an API, undesirable things
 // that are not necessarily wrong.
 func (l *Logger) Warning(message interface{}) {
 	l.AddRecord(types.WARNING, l.String(message))
 }
 
-// Runtime errors that do not require immediate action but should typically
+// Error Runtime errors that do not require immediate action but should typically
 // be logged and monitored.
 func (l *Logger) Error(message interface{}) {
 	l.AddRecord(types.ERROR, l.String(message))
 }
 
-// Adds a log record at the ALERT level.
+// Alert Adds a log record at the ALERT level.
 func (l *Logger) Alert(message interface{}) {
 	l.AddRecord(types.ALERT, l.String(message))
 }
 
-// System is unusable.
+// Emergency System is unusable.
 func (l *Logger) Emergency(message interface{}) {
 	l.AddRecord(types.EMERGENCY, l.String(message))
 }
 
-// Set the timezone to be used for the timestamp of log records.
+// SetTimezone Set the timezone to be used for the timestamp of log records.
 func (l *Logger) SetTimezone(tz string) {
 	l.timezone = tz
 }
 
-// Get timezone
+// GetTimezone Get timezone
 func (l *Logger) GetTimezone(tz string) string {
 	return l.timezone
 }
 
-// Stringfy
+// String Stringfy
 func (l *Logger) String(data interface{}) string {
 	switch data.(type) {
 	case string:
